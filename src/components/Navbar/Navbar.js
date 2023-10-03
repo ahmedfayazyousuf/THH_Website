4e714cd249f0a1da73276245b4209857a4dd09e1
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-10-03 13:21:38 +0400

    o
    o

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,36 @@
 import React from 'react'; 
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from "react-router-dom";
-import thhblack from '../1_Assets/thhblack.png';
+import thhwhite from '../1_Assets/thhwhite.png';
 
 const Navbar = () => {
     const RenderMenu = () => {
             return (
                 <>
                     <li className="nav-item active">
-                        <NavLink className="nav-link" to="/Invited" style={{fontWeight: '600', color: 'black'}}>
+                        <NavLink className="nav-link" to="/Invited" style={{fontWeight: '600'}}>
                         About 
                         </NavLink>
                     </li>
                     <li className="nav-item" >
-                        <NavLink className="nav-link" to="/Fire" style={{fontWeight: '600', color: 'black'}}>
+                        <NavLink className="nav-link" to="/Fire" style={{fontWeight: '600'}}>
                         Fire
                         </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/Blogs" style={{fontWeight: '600', color: 'black'}}>
+                        <NavLink className="nav-link" to="/Blogs" style={{fontWeight: '600'}}>
                         Blogs
                         </NavLink>
                     </li>
 
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/Clients" style={{fontWeight: '600', color: 'black'}}>
+                        <NavLink className="nav-link" to="/Clients" style={{fontWeight: '600'}}>
                         Clients
                         </NavLink>
                     </li>
 
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/Contact" style={{fontWeight: '600', color: 'black'}}>
+                        <NavLink className="nav-link" to="/Contact" style={{fontWeight: '600'}}>
                         Contact
                         </NavLink>
                     </li>
@@ -38,9 +38,9 @@ const Navbar = () => {
             )
     }
     return (
-    <nav id='navigator' className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: 'transparent', padding: '0', margin: '0', color:'black'}} >   
+    <nav id='navigator' className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: 'transparent', padding: '0', margin: '0'}} >   
         <NavLink className="navbar-brand" to="/">
-            <img style={{marginLeft:'15px', width: '70px', height: '40px', marginTop: '5px', marginBottom: '5px'}} src={thhblack} alt="the hanging house logo"/>
+            <img style={{marginLeft:'15px', width: '70px', height: '40px', marginTop: '5px', marginBottom: '5px'}} src={thhwhite} alt="the hanging house logo"/>
         </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
