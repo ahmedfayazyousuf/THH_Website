c71c2b1a64a9dade4446cbf4241985eea7590404
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-10-04 09:21:55 +0400

    o
    o

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -69,6 +69,63 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
+
+            <section id="contact" style={{borderRadius: '10px'}}>
+                <div class="contact-box" style={{borderRadius: '10px'}}>
+                    <div class="contact-links">
+                        <h2>CONTACT</h2>
+                        <div class="links">
+                            <div class="link">
+                                <a>
+                                    <img className='imgx' src="https://i.postimg.cc/m2mg2Hjm/linkedin.png" alt="linkedin"/>
+                                </a>
+                            </div>
+
+                            <div class="link">
+                                <a>
+                                    <img className='imgx' src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github"/>
+                                </a>
+                            </div>
+
+                            <div class="link">
+                                <a>
+                                    <img className='imgx' src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github"/>
+                                </a>
+                            </div>
+
+                            <div class="link">
+                                <a>
+                                    <img className='imgx' src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="contact-form-wrapper" style={{backgroundColor: 'transparent'}}>
+                    <form>
+                        <div class="form-item">
+                            <input type="text" name="sender" required/>
+                            <label>Name:</label>
+                        </div>
+                        
+                        <div class="form-item">
+                            <input type="text" name="email" required/>
+                            <label>Email:</label>
+                        </div>
+                        
+                        <div class="form-item">
+                            <textarea class="" name="message" required></textarea>
+                            <label>Message:</label>
+                        </div>
+                        <button class="submit-btn">Send</button>  
+                    </form>
+                    </div>
+                </div>
+                </section>
+
+
+
         </>
     )
 }
