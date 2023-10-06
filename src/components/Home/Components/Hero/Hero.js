79280b98f1ad65c2a7d948d4c074d3ae3644744f
Adithya Suresh <55681300+AdiS0928@users.noreply.github.com>
2023-10-06 17:42:04 +0400

    comit

--- src/components/Home/Components/Hero/Hero.js
+++ src/components/Home/Components/Hero/Hero.js
@@ -30,70 +30,70 @@ export default function Hero(){
         <div style={{background: 'grey', height: '100vh', width: '100vw', display:'flex', justifyContent: 'center', alignItems: 'center',position:'relative'}}>
             <div id="slideHero">
                 <div class="item" style={{backgroundImage: `url('${NissanXTrail}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}>
                         <div class="name">Nissan Al Rostamani – X-Trail Launch 2023</div>
                         <div class="des">A Thrilling X-Pedition into the Unknown</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${Geely}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}>
                         <div class="name">GEELY X AGMC – UAE Showroom Launch 2023</div>
                         <div class="des">Navigating the Future of Mobility</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${Huawei}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}>
                         <div class="name">Huawei – Comic Con 2023</div>
                         <div class="des">Encouraging Business Innovations</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${NBA}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">NBA2KL – Comic Con 2023</div>
                         <div class="des">Welcome to Swish City</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${NissanEmployee}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">Nissan Middle East FZE – Employee Event</div>
                         <div class="des">Explore More, Together</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${ArabHealth}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">American Hospital – Arab Health 2023</div>
                         <div class="des">Bringing Evolution in Motion</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${AlSerkal}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">Al Serkal Avenue – Quoz Arts Fest</div>
-                        <div class="des">A Celebration of Color & Connection/div>
+                        <div class="des">A Celebration of Color & Connection </div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${NissanAftersales}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">Nissan Middle East FZE – Aftersales Conference</div>
                         <div class="des">A Comprehensive Conference</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${G42}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">G42 at Gitex 2022</div>
                         <div class="des">Discover Limitless</div>
                         <button>See more</button>
                     </div>
                 </div>
                 <div class="item" style={{backgroundImage: `url('${RTA}')`}}>
-                    <div class="content">
+                    <div class="content" style={{zIndex:'1000000000000000'}}> 
                         <div class="name">Roads & Transport Authority at Project AB</div>
                         <div class="des">Expanding Horizons</div>
                         <button>See more</button>
@@ -106,7 +106,8 @@ export default function Hero(){
                 <button id="next"><i class="fa-solid fa-angle-right"></i></button>
             </div>
 
-            </div>
         </div>
+
+
     )
 }
