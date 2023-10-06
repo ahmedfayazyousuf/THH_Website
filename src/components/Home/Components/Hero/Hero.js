d086572c66193d7ec2c321d6983431d9a5cabb00
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-10-06 17:31:47 +0400

    o
    o

--- src/components/Home/Components/Hero/Hero.js
+++ src/components/Home/Components/Hero/Hero.js
@@ -2,6 +2,17 @@ import {React,useEffect} from 'react';
 import '../../../1_Assets/thh.css';
 import NissanXTrail from './../../../1_Assets/Images/Projects/NissanXTrail.jpg';
 import Geely from './../../../1_Assets/Images/Projects/Geely.jpg';
+import Huawei from './../../../1_Assets/Images/Projects/Huawei.jpg';
+
+
+import NBA from './../../../1_Assets/Images/Projects/NBA.jpg';
+import NissanEmployee from './../../../1_Assets/Images/Projects/NissanEmployee.png';
+import ArabHealth from './../../../1_Assets/Images/Projects/ArabHealth.jpg';
+import AlSerkal from './../../../1_Assets/Images/Projects/AlSerkal.jpg';
+import NissanAftersales from './../../../1_Assets/Images/Projects/NissanAftersales.png';
+import G42 from './../../../1_Assets/Images/Projects/G42.jpg';
+import RTA from './../../../1_Assets/Images/Projects/RTA.jpg';
+
 
 export default function Hero(){
     useEffect(()=>{
@@ -32,39 +43,69 @@ export default function Hero(){
                         <button>See more</button>
                     </div>
                 </div>
-                <div class="item" style={{backgroundImage: `url('${NissanXTrail}')`}}>
+                <div class="item" style={{backgroundImage: `url('${Huawei}')`}}>
                     <div class="content">
-                        <div class="name">LUNDEV</div>
-                        <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+                        <div class="name">Huawei – Comic Con 2023</div>
+                        <div class="des">Encouraging Business Innovations</div>
                         <button>See more</button>
                     </div>
                 </div>
-                <div class="item" style={{backgroundImage: `url('${NissanXTrail}')`}}>
+                <div class="item" style={{backgroundImage: `url('${NBA}')`}}>
                     <div class="content">
-                        <div class="name">LUNDEV</div>
-                        <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+                        <div class="name">NBA2KL – Comic Con 2023</div>
+                        <div class="des">Welcome to Swish City</div>
                         <button>See more</button>
                     </div>
                 </div>
-                <div class="item" style={{backgroundImage: `url('${NissanXTrail}')`}}>
+                <div class="item" style={{backgroundImage: `url('${NissanEmployee}')`}}>
                     <div class="content">
-                        <div class="name">LUNDEV</div>
-                        <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+                        <div class="name">Nissan Middle East FZE – Employee Event</div>
+                        <div class="des">Explore More, Together</div>
                         <button>See more</button>
                     </div>
                 </div>
-                <div class="item" style={{backgroundImage: `url('${NissanXTrail}')`}}>
+                <div class="item" style={{backgroundImage: `url('${ArabHealth}')`}}>
                     <div class="content">
-                        <div class="name">LUNDEV</div>
-                        <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
+                        <div class="name">American Hospital – Arab Health 2023</div>
+                        <div class="des">Bringing Evolution in Motion</div>
+                        <button>See more</button>
+                    </div>
+                </div>
+                <div class="item" style={{backgroundImage: `url('${AlSerkal}')`}}>
+                    <div class="content">
+                        <div class="name">Al Serkal Avenue – Quoz Arts Fest</div>
+                        <div class="des">A Celebration of Color & Connection/div>
+                        <button>See more</button>
+                    </div>
+                </div>
+                <div class="item" style={{backgroundImage: `url('${NissanAftersales}')`}}>
+                    <div class="content">
+                        <div class="name">Nissan Middle East FZE – Aftersales Conference</div>
+                        <div class="des">A Comprehensive Conference</div>
+                        <button>See more</button>
+                    </div>
+                </div>
+                <div class="item" style={{backgroundImage: `url('${G42}')`}}>
+                    <div class="content">
+                        <div class="name">G42 at Gitex 2022</div>
+                        <div class="des">Discover Limitless</div>
+                        <button>See more</button>
+                    </div>
+                </div>
+                <div class="item" style={{backgroundImage: `url('${RTA}')`}}>
+                    <div class="content">
+                        <div class="name">Roads & Transport Authority at Project AB</div>
+                        <div class="des">Expanding Horizons</div>
                         <button>See more</button>
                     </div>
                 </div>
             </div>
-
+            
             <div class="buttons">
                 <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
                 <button id="next"><i class="fa-solid fa-angle-right"></i></button>
+            </div>
+
             </div>
         </div>
     )
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react';
+import {React} from 'react';
 import '../1_Assets/thh.css';
 import ContactForm from './Components/Contact/Contact'
 import BlogGallery from './Components/BlogGallery/BlogGallery'
@@ -8,55 +8,11 @@ import Hero from './Components/Hero/Hero';
 
 const Home = () => { 
 
-    useEffect(()=>{
-        const cursor = document.getElementById("cursor");
-
-        const animateCursor = (e, interacting) => {
-          const x = e.clientX - cursor.offsetWidth / 2,
-                y = e.clientY - cursor.offsetHeight / 2;
-          
-          const keyframes = {
-            transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
-          }
-          
-          cursor.animate(keyframes, { 
-            duration: 800, 
-            fill: "forwards" 
-          });
-        }
-        
-        const getCursorClass = type => {
-          switch(type) {
-            case "video":
-              return "play";
-              case "image":
-              return "fa-solid fa-pause";
-            default:
-              return "fa-solid fa-arrow-up-right"; 
-          }
-        }
-        
-        window.onmousemove = e => {
-          const interactable = e.target.closest(".interactable"),
-                interacting = interactable !== null;
-          
-          const icon = document.getElementById("cursor-icon");
-          
-          animateCursor(e, interacting);
-          
-          cursor.dataset.type = interacting ? interactable.dataset.type : "";
-          
-          if(interacting) {
-            icon.className = getCursorClass(interactable.dataset.type);
-          }
-        }
-    },[])
-
     return (
         <div style={{width: '100vw', height: '100%'}}>
-            <div id="cursor">
-                <i id="cursor-icon" class="fa-solid fa-arrow-up-right"></i>
-            </div>
+
+            
+
             <div style={{background: 'grey', height: '100vh', width: '100vw', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <Hero />
             </div>
@@ -76,10 +32,16 @@ const Home = () => {
             <div style={{background: 'black', height: '100%', width: '100vw', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <ClientsCarousel/>
             </div>
+
+            <div class="tooltip" style={{color: 'white', zIndex: '1000000000000000000000000', width: '100vw', height: '100px', background: 'red'}}>Hover over me
+              <p class="tooltiptext" style={{color: 'white'}}>Tooltip text</p>
+            </div>
             
             <div style={{background: 'black', height: '100%', width: '100vw', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <ContactForm/>
             </div>
+
+            
         </div>
     )
 }
